67803a61dac1deddb3d1c667b5cdb38fa663ca1e
syswu <wuheng@iscas.ac.cn>
2019-01-09 22:31:13 +0800

    add ParamAndRespPatternAnalyzer

---
+++ wukong-core/src/main/java/com/github/wukong/core/anas/ParamAndRespPatternAnalyzer.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (2018-2019) Institute of Software, Chinese Academy of Sciences 
+ */
+package com.github.wukong.core.anas;
+
+import java.lang.reflect.Method;
+
+import com.github.wukong.core.KindAnalyzer;
+
+/**
+ * @author wuheng@otcaix.iscas.ac.cn
+ * @since  2019.1
+ */
+public abstract class ParamAndRespPatternAnalyzer extends KindAnalyzer {
+
+	@Override
+	protected boolean isKind(Method method) {
+		if (method == null 
+				|| getSuperclass() == null) {
+			return false;
+		}
+		
+		if (method.isAnnotationPresent(Deprecated.class)) {
+			return false;
+		}
+		
+		if (method.getParameterCount() != 1) {
+			return false;
+		}
+
+		Class<?> paramType = method.getParameterTypes()[0];
+		for (Class<?> clazz : paramType.getInterfaces()) {
+			if (getSuperclass().equals(clazz.getName())) {
+				return true;
+			}
+		}
+		
+		if( paramType.getSuperclass() != null && getSuperclass().equals(
+					paramType.getSuperclass().getName())) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	protected boolean isKindGroup(Method method) {
+		return false;
+	}
+
+	/**
+	 * 根据名字方法名返回kind类型，如果不存在，返回null
+	 * 
+	 */
+	@Override
+	protected String toKind(Method method) {
+		String simpleName = method.getName();
+		return simpleName.substring(0, 1).toUpperCase() + simpleName.substring(1);
+	}
+
+	@Override
+	protected String toModel(Method method) {
+		return method.getParameterTypes()[0].getTypeName();
+	}
+	
+	/**
+	 * We're not gonna check if it's empty.
+	 * 
+	 * @return superclass
+	 */
+	protected abstract String getSuperclass();
+}
--- wukong-core/src/main/java/com/github/wukong/core/anas/ReqAndRespPatternAnalyzer.java
+++ wukong-core/src/main/java/com/github/wukong/core/anas/ReqAndRespPatternAnalyzer.java
@@ -11,7 +11,7 @@ import com.github.wukong.core.KindAnalyzer;
  * @author wuheng@otcaix.iscas.ac.cn
  * @since  2019.1
  */
-public abstract class CommonKindAnalyzer extends KindAnalyzer {
+public abstract class ReqAndRespPatternAnalyzer extends KindAnalyzer {
 
 	@Override
 	protected boolean isKind(Method method) {
@@ -44,21 +44,11 @@ public abstract class CommonKindAnalyzer extends KindAnalyzer {
 		return false;
 	}
 
-	/**
-	 * 对于fabric8的DefaultKubernetesClient而言，如果返回值是以GroupDSL结尾的， 则说明它是一种kind的类型. <br>
-	 * <br>
-	 * 
-	 * 更进一步，要求这些方法不是<code>Deprecated.class</code>的类型
-	 */
 	@Override
 	protected boolean isKindGroup(Method method) {
 		return false;
 	}
 
-	/**
-	 * 根据名字方法名返回kind类型，如果不存在，返回null
-	 * 
-	 */
 	@Override
 	protected String toKind(Method method) {
 		String simpleName = method.getReturnType().getSimpleName();
