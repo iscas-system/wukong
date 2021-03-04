dbabec7a4a6269ed535ec0cb855f7d4feb12a256
Heng WU <wuheng@iscas.ac.cn>
2021-03-04 16:16:48 +0800

    fix trigger bug

--- trigger/src/main/java/com/github/doslab/wukong/HttpRequester.java
+++ trigger/src/main/java/com/github/doslab/wukong/HttpRequester.java
@@ -42,12 +42,13 @@ public class HttpRequester {
 
 	public static final String DATETIME_STR_END = "</td></tr>";
 
-	public static final String MAVEN_URL_PREFIX = "https://mvnrepository.com/artifact/";
+	public static final String MAVEN_URL_PREFIX = "https://repo.maven.apache.org/maven2/";
 
-	public static final String DEFAULT_GROUPID = "com.aliyun";
+	public static final String DEFAULT_GROUPID = "com.amazonaws";
 
-	public static final String DEFAULT_ARTIFCAT = "aliyun-java-sdk-ecs";
+	public static final String DEFAULT_ARTIFCAT = "aws-java-sdk-ec2";
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws Exception {
 
 		Class.forName("org.postgresql.Driver").newInstance();
@@ -56,30 +57,29 @@ public class HttpRequester {
 
 		String groupId = System.getenv("groupId") == null ? DEFAULT_GROUPID : System.getenv("groupId");
 		String artifactId = System.getenv("artifactId") == null ? DEFAULT_ARTIFCAT : System.getenv("artifactId");
-		String fullUrl = MAVEN_URL_PREFIX + groupId + "/" + artifactId;
+		String fullUrl = MAVEN_URL_PREFIX + groupId.replace(".", "/") + "/" + artifactId;
 		CloseableHttpClient client = createDefaultHttpClient();
 		HttpGet request = new HttpGet(new URI(fullUrl));
 		HttpEntity entity = client.execute(request).getEntity();
 		BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
-		String line = null;
-		while ((line = br.readLine()) != null) {
-			if (line.contains(artifactId) && line.contains(VERSION_STR_START)) {
-				try {
-					
-					String version = StringUtils.toMiddleString(line, VERSION_STR_START, VERSION_STR_END);
-					PreparedStatement ps = conn.prepareStatement("INSERT INTO versions(groupid,artifactid,version,release) values(?,?,?,?)");
-					ps.setString(1, groupId);
-					ps.setString(2, artifactId);
-					ps.setString(3, version);
-					String dateTime = StringUtils.toMiddleString(line, DATETIME_STR_START, DATETIME_STR_END);
-					String[] splits = dateTime.split(",");
-					ps.setTimestamp(4, new Timestamp(Integer.parseInt(splits[1].trim()) - 1900, 
-							StringUtils.toMonth(splits[0]), 1, 0, 0, 0, 0));
-					ps.executeUpdate();
-				} catch (Exception ex) {
-					System.out.println(ex);
-
-				}
+		String fullline = null;
+		while ((fullline = br.readLine()) != null) {
+			if (fullline.contains("href=") && fullline.indexOf("..") == -1 && fullline.indexOf("xml") == -1) {
+				int stx = fullline.indexOf("\"");
+				int edx = fullline.indexOf("/\"");
+				String version = fullline.substring(stx + 1, edx);
+				String datatime = fullline.split("\\s+")[3].trim();
+				PreparedStatement ps = conn.prepareStatement("INSERT INTO versions(groupid,artifactid,version,release) values(?,?,?,?)");
+				ps.setString(1, groupId);
+				ps.setString(2, artifactId);
+				ps.setString(3, version);
+				String[] splits = datatime.split("-");
+				ps.setTimestamp(4, new Timestamp(
+						Integer.parseInt(splits[0].trim()) - 1900, 
+						Integer.parseInt(splits[1].trim()), 
+						Integer.parseInt(splits[2].trim()), 
+						0, 0, 0, 0));
+				ps.executeUpdate();
 			}
 		}
 	}
