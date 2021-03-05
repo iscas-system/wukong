73d722d740e396d4313b1f2cf0a461258327d9a1
Heng WU <wuheng@iscas.ac.cn>
2021-03-05 13:08:48 +0800

    fix bug

--- trigger/src/main/java/com/github/doslab/wukong/NewAPIAnalyzer.java
+++ trigger/src/main/java/com/github/doslab/wukong/NewAPIAnalyzer.java
@@ -30,25 +30,30 @@ public class NewAPIAnalyzer extends AbstractAnalyzer {
 	public void analyse() throws Exception {
 		for (File file : new File("jsons").listFiles()) {
 
-			CloudMetadata ccm = new ObjectMapper().readValue(file, CloudMetadata.class);
-			CloudAPIAnalyzer cmd = new CloudAPIAnalyzer(ccm, new CloudClassloader(ccm));
-			Map<String, JsonNode> nodes = cmd.extractCloudAPIs();
-			for (String key : nodes.keySet()) {
-				try {
-					ps.setString(1, ccm.kind);
-					String artifactId = ccm.getDependency().get(0).getArtifactId();
-					ps.setString(2, artifactId);
-					String version = ccm.getDependency().get(0).getVersion();
-					ps.setString(3, version);
-					ps.setString(4, key.toLowerCase());
-					ps.setInt(5, 0);
-					ps.executeUpdate();
-					System.out.println("insert " + ccm.kind + "," + version + "," + key.toLowerCase());
-				} catch (Exception ex) {
-					System.out.println(ex);
-					break;
-				}
+			try {
+				CloudMetadata ccm = new ObjectMapper().readValue(file, CloudMetadata.class);
+				CloudAPIAnalyzer cmd = new CloudAPIAnalyzer(ccm, new CloudClassloader(ccm));
+				Map<String, JsonNode> nodes = cmd.extractCloudAPIs();
+				for (String key : nodes.keySet()) {
+					try {
+						ps.setString(1, ccm.kind);
+						String artifactId = ccm.getDependency().get(0).getArtifactId();
+						ps.setString(2, artifactId);
+						String version = ccm.getDependency().get(0).getVersion();
+						ps.setString(3, version);
+						ps.setString(4, key.toLowerCase());
+						ps.setInt(5, 0);
+						ps.executeUpdate();
+						System.out.println("insert " + ccm.kind + "," + version + "," + key.toLowerCase());
+					} catch (Exception ex) {
+						System.out.println(ex);
+						break;
+					}
 
+				}
+			} catch (Exception ex) {
+				System.out.println(ex);
+				continue;
 			}
 
 		}
